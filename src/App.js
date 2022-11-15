28c9ea84adfb2eaaf028d0b07d9f6970e8014cf6
Luca Liebenberg <liebenbergluca@gmail.com>
2022-11-15 11:22:43 +0200

    Carousel component

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Hero from "./components/Hero";
 import Navbar from "./components/Navbar";
 import Search from "./components/Search";
 import Selects from "./components/Selects";
+import Carousel from "./components/Carousel";
 
 function App() {
   return (
@@ -13,6 +14,7 @@ function App() {
       <Destinations />
       <Search />
       <Selects />
+      <Carousel />
     </div>
   );
 }
